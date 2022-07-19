cbcfd50965f908917275efaa79cdc99f3a9bacb0
Bimaekap <61057669+Bimaekap@users.noreply.github.com>
2022-07-19 19:21:08 +0700

    Added

---
+++ dist/authentication.js
@@ -0,0 +1,58 @@
+// export to main.js
+
+export function getAuthentication() {
+    return getInside(), getToken();
+}
+
+
+// import from getToken.js
+import {
+    getTokenInvoke
+} from "./getCode.js";
+//--------------------------
+
+// let name = 'this is code challange';
+// console.log(name + getTokenInvoke())
+
+
+const api_authentication = 'https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=9e04967f457b1e0951e3faadc808a242&code_challenge=8638c94eec94106dfb01e0cafd0a300556e72b4826431d2c9f809c7fd21254d7';
+const api_url_token = 'https://myanimelist.net/v1/oauth2/token'
+const client_code = {
+    theID: '9e04967f457b1e0951e3faadc808a242',
+    theSecretID: '8638c94eec94106dfb01e0cafd0a300556e72b4826431d2c9f809c7fd21254d7'
+}
+
+let CLIENT_ID = client_code.theID;
+let CLIENT_SECRET = client_code.theSecretID;
+
+const getInside = async () => {
+    const response = await fetch(api_authentication, {
+        mode: 'no-cors',
+        client_id: CLIENT_ID,
+    })
+    const data = response.text()
+    console.log(data)
+    return data
+
+}
+
+const getToken = async () => {
+    const response_url = await fetch(api_url_token, {
+        method: 'POST',
+        client_id: CLIENT_ID,
+        mode: 'no-cors',
+        state: 'http://127.0.0.1:5500/index.html',
+        client_secret: CLIENT_SECRET,
+        grant_type: 'authorization_code',
+        code: 'authorization_code',
+        redirect_url: 'http://127.0.0.1:5500/index.html',
+        code_verifier: getTokenInvoke(),
+    })
+
+    const token_url = await response_url.text()
+    console.log(token_url)
+    const tokenAdd = getTokenInvoke()
+    const token = await tokenAdd.text()
+    console.log(token)
+    
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+//import from authentication.js
+import { getAuthentication } from "./dist/authentication.js";
+
+const api_url = 'https://api.myanimelist.net/v2/anime?q=one&limit=4';
+
+let myToken = getAuthentication();
+
+console.log(myToken)
+const getAPI = async () => {
+    const response = await fetch(api_url,{
+        method:'GET',
+        mode:'no-cors',
+        credentials:'include',
+        headers:new Headers({
+            'X-MAL-CLIENT-ID':'9e04967f457b1e0951e3faadc808a242',
+            'Authorization' : myToken,            
+        })
+    })
+}
+
+
+// CALL API
+
+getAPI();
