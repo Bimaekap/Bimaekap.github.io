fbb0aaa35419681d00b33fad9a35fd57cdb14374
Bimaekap <61057669+Bimaekap@users.noreply.github.com>
2022-07-20 22:44:12 +0700

    Update authentication.js

--- docs/dist/authentication.js
+++ docs/dist/authentication.js
@@ -9,9 +9,6 @@ import { authorization_code } from "./authorization_code.js";
 
 import { code_generator } from "./code_generator.js";
 
-const code_challenge = code_generator();
-const api_authentication = `https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=9e04967f457b1e0951e3faadc808a242&code_challenge=${code_challenge}`;
-
 const client_code = {
     theID: '9e04967f457b1e0951e3faadc808a242',
     theSecretID: '8638c94eec94106dfb01e0cafd0a300556e72b4826431d2c9f809c7fd21254d7'
@@ -21,6 +18,10 @@ const client_code = {
 let CLIENT_ID = client_code.theID;
 let CLIENT_SECRET = client_code.theSecretID;
 
+const code_challenge = code_generator();
+const api_authentication = `https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&code_challenge=${code_challenge}`;
+console.log(code_challenge)
+
 // data for injection token 
 
 
@@ -29,6 +30,9 @@ const getAuthentication = async () => {
         mode: 'no-cors',
         client_id: CLIENT_ID,
     })
+    console.log(`authorize by clicking here + ${window.location.href}`)
+
+    return response
 }
 
 authorization_code();
