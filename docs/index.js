552c1735187122c9233f198bc944add4f8b5d6a6
Bimaekap <61057669+Bimaekap@users.noreply.github.com>
2022-07-22 17:04:47 +0700

    upd

--- docs/index.js
+++ docs/index.js
@@ -34,6 +34,7 @@ const authorization_request = async () => {
 
 const get_token = await fetch(url_token,{
     method:'POST',
+    mode:'no-cors',
     client_id:client_id,
     grant_type:'authorization_code',
     code:authorization_code,
